5b8b3a43fc59c8b8697ed77e212743ac9b7bc12b
Cedric Hombourger <chombourger@gmail.com>
2015-11-14 00:35:02 +0300

    building of busybox networking tools is now optional
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

--- build/components/bb.mk
+++ build/components/bb.mk
@@ -1,6 +1,19 @@
+
+bb_net_keys_yes := 				\
+	CONFIG_IFCONFIG				\
+	CONFIG_UDHCPC				\
+	CONFIG_WGET				\
+	CONFIG_FEATURE_WGET_STATUSBAR		\
+	CONFIG_FEATURE_WGET_AUTHENTICATION	\
+
+bb_net_keys := $(bb_net_keys_$(strip $(USE_NETWORKING)))
+
 $(outdir)/bb/.config: build/components/bb.cfg
 	$(MKDIR_P) $(dir $@)
 	install -m 644 $< $@
+	$(foreach k,$(bb_net_keys),				\
+		sed -i -e 's,# $(k) is not set,$(k)=y,g' $@ &&	\
+	) true
 
 out/target/bb/Makefile: $(outdir)/bb/.config
 	$(MAKE) -C external/busybox O=$(CURDIR)/$(outdir)/bb CC="$(CC)" CFLAGS="$(CFLAGS)" oldconfig
